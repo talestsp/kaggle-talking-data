026fbaa6ee6b9ce3f0f673d3ee9586519f8d5938
Tales Pimentel <tales.tsp@gmail.com>
2016-07-20 00:32:35 -0300

    Full preprocessing

--- preproc/merge_datasets.py
+++ preproc/merge_datasets.py
@@ -5,29 +5,59 @@ import gc
 working_dir = "/home/tales/development/kaggle-talking-data/"
 chdir(working_dir)
 
-dtypes = {'label_id': str, 'category': str, "app_id": str, "event_id": str, "app_id": str, "is_installed": str,  "is_active": str, "gender": str ,"age": int, "group": str}
+dtypes = {'label_id': int, 'category': str, "event_id": str, "device_id": str, "app_id": long, "is_installed": str,  "is_active": str, "gender": str ,"age": int, "group": str, "gender": str, "age": int, "group": str}
 
 app_labels = pd.read_csv("data/app_labels.csv", dtype=dtypes)
-label_categ = pd.read_csv("data/label_categories.csv", dtype=dtypes)
+len(app_labels)
+app_labels = app_labels.drop_duplicates()
+len(app_labels)
 
-app_categ = pd.merge(app_labels, label_categ, on='label_id')
+label_categ = pd.read_csv("data/label_categories.csv", dtype=dtypes)
+len(label_categ)
+label_categ = label_categ.drop_duplicates()
+len(label_categ)
+
+app_categ = pd.merge(app_labels, label_categ, on='label_id', how='left')
+len(app_categ)
+app_categ = app_categ.drop_duplicates()
+len(app_categ)
+
 del app_categ['label_id']
 app_labels = None
 label_categ = None
 gc.collect()
 
-
 app_events = pd.read_csv("data/app_events.csv", dtype=dtypes)
-app_events = pd.merge(app_events, app_categ, on='app_id')
+app_events = pd.merge(app_events, app_categ, on='app_id', how='left')
 del app_events['app_id']
+app_categ = None
 gc.collect()
 app_events.head()
-app_events.to_csv("data/app_events_merging.csv", index=False, sep=";")
-
-app_event_groupby = app_events.groupby(['event_id', 'is_installed', 'is_active', 'category'])
-result = app_event_groupby[['event_id', 'is_installed', 'is_active', 'category']].agg(['count'])
+len(app_events)
+app_events = app_events.drop_duplicates()
+len(app_events)
+del app_events['is_installed']
+gc.collect()
+app_events.to_csv("data/app_events_merging_pd.csv", index=False, sep=";")
 
 events = pd.read_csv("data/events.csv", dtype=dtypes)
-app_events = pd.merge(app_events, events, on='event_id')
+app_events = pd.merge(app_events, events, on='event_id', how='left')
+del app_events['event_id']
+events = None
+gc.collect()
+app_events.head()
 
-#incomplete...
+gender_age = pd.read_csv("data/gender_age_train.csv", dtype=dtypes)
+gender_age.head()
+app_events = pd.merge(app_events, gender_age, on='device_id', how='left')
+app_events.head()
+
+phone_brand = pd.read_csv("data/phone_brand_device_model.csv", dtype=dtypes)
+app_events = pd.merge(app_events, phone_brand, on='device_id', how='left')
+del app_events['device_id']
+gender_age = None
+phone_brand = None
+gc.collect()
+app_events.head()
+
+app_events.to_csv("data/app_events_full_basic.csv", index=False, sep=";")
