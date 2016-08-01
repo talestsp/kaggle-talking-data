2ca8ac8a8e632c1f1d1cfcb8a0aa166ac3ef8d07
Tales Pimentel <tales.tsp@gmail.com>
2016-08-01 03:25:47 -0300

    Path size script done! It builds a file with the device, path_size divided by amount of days, and total path_size.

--- preproc/devices_path_size.py
+++ preproc/devices_path_size.py
@@ -1,7 +1,14 @@
+#this script returns the path size mean divided by number of days
+#it was used the euclidean distance
+
 import pandas as pd
 from os import chdir
 import gc
 import sys
+working_dir = "/home/tales/development/kaggle-talking-data/"
+data_dir = "data/"
+dataset = "train"
+chdir(working_dir)
 sys.path.append("preproc/libs/")
 from location_path import GeoPath
 
@@ -15,19 +22,34 @@ def remove_bad_locations(events):
 
 def partition_weekend(events):
 	weekend_config = (events["weekday"] == "sunday") | (events["weekday"] == "saturday") | ( (events["weekday"] == "friday") & (events["daytime"] == "dinner") ) | ( (events["weekday"] == "friday") & (events["daytime"] == "night" ) )
-	ev_week    = events[ ~weekend_config ]
+	ev_week = events[ ~weekend_config ]
 	del ev_week["daytime"]
 	del ev_week["weekday"]
+
 	ev_weekend = events[ weekend_config ]
 	del ev_weekend["daytime"]
 	del ev_weekend["weekday"]
+
 	return {"week": ev_week, "weekend": ev_weekend}
 
-working_dir = "/home/tales/development/kaggle-talking-data/"
-data_dir = "data/"
-dataset = "train"
+def devices_path_weight(ev):
+	devices = ev.device_id.drop_duplicates()
+	devices_path_weight = []
+	counter = 0
+	len_devices = len(devices)
+	
+	for dev in devices:
+		counter = counter + 1
+		print len_devices, counter
+		dev_df = ev[ ev["device_id"] == dev ]
+		dev_df = dev_df.sort_values(['device_id', 'timestamp'], ascending=[True, True])
+	
+		dev_gp = GeoPath(dev_df)
+		path_weight = dev_gp.path_weight()
+		n_days = len(dev_gp.distinct_days(dev_df.timestamp))
 
-chdir(working_dir)
+		devices_path_weight.append({"device_id": dev, "path_weight": path_weight, "path_weight_by_days": path_weight / n_days})
+	return devices_path_weight
 
 dtypes = {'label_id': int, 'category': str, "event_id": str, "device_id": str, "app_id": long, "is_installed": str,  "is_active": str, "gender": str ,"age": int, "group": str, "group": str, "phone_brand": str, "device_model": str, "daytime": str, "weekday": str}
 
@@ -46,8 +68,12 @@ ev_week = ev_week.sort_values(['device_id', 'timestamp'], ascending=[True, True]
 ev_weekend = events_partition["weekend"]
 ev_weekend = ev_weekend.sort_values(['device_id', 'timestamp'], ascending=[True, True])
 
+path_week = devices_path_weight(ev_week)
+path_weekend = devices_path_weight(ev_weekend)
 
-#checking GeoPath
-#a = ev_week[37:49]
-#gp = GeoPath(a)
+#path_week = pd.DataFrame(path_week)
+path_weekend = pd.DataFrame(path_weekend)
+
+#path_week.to_csv(data_dir + "path_weight_week.csv")
+path_weekend.to_csv(data_dir + "path_weight_weekend.csv")
 
--- preproc/libs/location_path.py
+++ preproc/libs/location_path.py
@@ -6,7 +6,7 @@ class GeoPath:
 
 	def __init__(self, sorted_locations):
 		points_seq = self.build_points(sorted_locations)
-		self.edges = self.build_edges(points_seq)
+		self.edges = self.build_path(points_seq)
 
 	def build_points(self, sorted_locations):
 		points_seq = []
@@ -19,6 +19,15 @@ class GeoPath:
 
 		return points_seq
 
+	def distinct_days(self, timestamps):
+		days = []
+		for item in timestamps.iteritems():
+			item = item[1]
+			ts = item.split(" ")[0]
+			days.append(ts)
+			
+		return set(days)
+
 	def distance(self, p1, p2, method="euclidean"):
 		if (method == "euclidean"):
 			dist = sqrt( float( (p2[0] - p1[0])**2 + (p2[1] - p1[1])**2 ) )
@@ -26,19 +35,26 @@ class GeoPath:
 			raise Exception("Method not implemented")
 		return dist
 
-	def build_edges(self, points_seq, decimal_precision=1):
+	def build_path(self, points_seq, decimal_precision=6, skip_if_bellow=0.03):
 		last_point = points_seq[0]
 		path = []
+
 		for point in points_seq:
-			edge_weight = round(self.distance(point, last_point), decimal_precision)
-			edge = last_point, point, edge_weight
-			path.append(edge)
-			last_point = point
+			edge_weight = self.distance(point, last_point)
+		
+			if (edge_weight > skip_if_bellow):
+				edge_weight = round(edge_weight, decimal_precision)
+				edge = last_point, point, edge_weight
+				path.append(edge)
+				last_point = point
+
 		return path
 
 	def path_weight(self):
 		path_weight = 0
+		
 		for edge in self.edges:
 			edge = edge[2]
 			path_weight = path_weight + edge
-		return path_weight
+		
+		return float(path_weight)
