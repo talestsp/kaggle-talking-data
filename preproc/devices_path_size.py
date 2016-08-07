b4a3b76f1502234cbaa64d4085acc2b5da81d523
Tales Pimentel <tales.tsp@gmail.com>
2016-08-07 05:22:33 -0300

    Some print feedbacks

--- preproc/devices_path_size.py
+++ preproc/devices_path_size.py
@@ -63,17 +63,17 @@ ev_week = ev_week.sort_values(['device_id', 'timestamp'], ascending=[True, True]
 ev_weekend = events_partition["weekend"]
 ev_weekend = ev_weekend.sort_values(['device_id', 'timestamp'], ascending=[True, True])
 
-#path_week = devices_path_weight(ev_week)
+path_week = devices_path_weight(ev_week)
 path_weekend = devices_path_weight(ev_weekend)
 
-#path_week = pd.DataFrame(path_week)
+path_week = pd.DataFrame(path_week)
 path_weekend = pd.DataFrame(path_weekend)
 
-#path_week.to_csv(data_dir + "path_weight_week.csv")
+path_week.to_csv(data_dir + "path_weight_week.csv")
 path_weekend.to_csv(data_dir + "path_weight_weekend-002.csv")
 
-#path_week.path_weight.describe()
-#path_week.path_weight_by_days.describe()
+path_week.path_weight.describe()
+path_week.path_weight_by_days.describe()
 
 path_weekend.path_weight.describe()
 path_weekend.path_weight_by_days.describe()
