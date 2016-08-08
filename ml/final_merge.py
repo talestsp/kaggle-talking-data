f9bc55167ef7eba70ee7c8216b01bd53a4320887
Tales Tenorio de Souza Pimentel <talestsp@users.noreply.github.com>
2016-08-08 20:36:09 -0300

    Update final_merge.py

--- ml/final_merge.py
+++ ml/final_merge.py
@@ -24,14 +24,14 @@ def load_path_weight(dataset):
 	path_weight = pd.merge(path_weight_week, path_weight_weekend, on='device_id', how='outer')
 	#
 	if(dataset == "train"):
-		data = pd.merge(devices_train, path_weight, on='device_id', how='left')
+		data = pd.merge(devices_train, path_weight, on='device_id', how='inner')
+		del data["group"]
 		del data["gender"]
 		del data["age"]
-		del data["group"]
 		return data
 	#
 	elif(dataset == "test"):
-		data =  pd.merge(devices_test, path_weight, on='device_id', how='left')
+		data =  pd.merge(devices_test, path_weight, on='device_id', how='inner')
 		return data
 	#
 	else:
@@ -40,6 +40,7 @@ def load_path_weight(dataset):
 def load_nn_location(dataset):
 	if (dataset == "train"):
 		data = pd.read_csv(data_dir + "/devices_nn_location_train.csv", dtype=dtypes, sep=",")
+		del data["group"]
 		del data["Unnamed: 0"]
 		return data
 	elif (dataset == "test"):
@@ -58,10 +59,18 @@ def merge_all(df_list, dataset):
 	for df in df_list:
 		final_df = pd.merge(final_df, df, on='device_id', how='left')
 	#
+	print df.columns.tolist()
 	return final_df
+
+def remove_na_rows(df):
+	columns = list(df.columns)
+	columns.remove("device_id")
+	return df.dropna(subset=columns, how="all")
+
 
 data_dir = "data/"
 working_dir = "/home/tales/development/kaggle-talking-data/"
+chdir(working_dir)
 
 dtypes = {'label_id': int, 'category': str, "event_id": str, "device_id": str, "app_id": long, "is_installed": str,  "is_active": str, "gender": str ,"age": int, "group": str, "group": str, "phone_brand": str, "device_model": str, "daytime": str, "weekday": str}
 
@@ -76,6 +85,9 @@ nn_loc_test = load_nn_location("test")
 
 data_train = merge_all([pw_train, nn_loc_train], "train")
 data_test = merge_all([pw_test, nn_loc_test], "test")
+
+data_train = remove_na_rows(data_train)
+data_test = remove_na_rows(data_test)
 
 data_train.to_csv(data_dir + "data_train.csv", sep = ";")
 data_test.to_csv(data_dir + "data_test.csv", sep = ";")
